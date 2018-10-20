61c1e1258cca1d5a2ac7ac656a81245e87e7ddf5
andres2906@gmail.com <andres2906@gmail.com>
2018-10-20 18:29:22 -0500

    visualizacion del mapa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    NgxEchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
